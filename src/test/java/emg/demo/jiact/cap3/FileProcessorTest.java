6f2b5225f78b3502cbe68151c54aad78924ca977
petrubear <edison.martinez@gmail.com>
2020-05-05 15:47:55 -0500

    feat: add predicates and function samples

--- src/test/java/emg/demo/jiact/cap3/FileProcessorTest.java
+++ src/test/java/emg/demo/jiact/cap3/FileProcessorTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.Test;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 
 public class FileProcessorTest {
@@ -21,6 +24,16 @@ public class FileProcessorTest {
 
         String twoLines = FileProcessor.processFile((BufferedReader br) -> br.readLine() + br.readLine(), file.getAbsolutePath());
         Assert.assertEquals(twoLines, "Line1Line2");
+
+        Function<String, String> upperCase = String::toUpperCase;
+        Assert.assertEquals(upperCase.apply(twoLines), twoLines.toUpperCase());
+
+        Predicate<String> isUpperCase = s -> s.equals(s.toUpperCase());
+        Predicate<String> isNotNull = Objects::nonNull;
+        Predicate<String> isNotEmpty = s -> !s.isEmpty();
+
+        Assert.assertTrue(isNotNull.or(isNotEmpty).and(isUpperCase).test(upperCase.apply(oneLine)));
+
     }
 
 }
